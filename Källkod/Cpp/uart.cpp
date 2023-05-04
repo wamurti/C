17a78057ac339598db3c2ca83d4a41e3836fc56a
wamurti <79321522+wamurti@users.noreply.github.com>
2023-05-04 08:56:19 +0200

    Update uart.cpp

--- Källkod/Cpp/uart.cpp
+++ Källkod/Cpp/uart.cpp
@@ -4,19 +4,26 @@ void USART2_Init(void) { //Deklarerar en funktion för att initiera USART-protko
 
 // 1. Enablea klocktillgång för uart2
 
-RCC->APB1ENR |= 0x20000; //Atkiverar UART2 genom att sätta bit 17 i APB1ENR till 1.
+RCC->APB1ENR |= 0x20000; //Atkiverar UART2 genom att sätta bit 17 (USART2 clock enable) i APB1ENR till 1.
 
-// 2. Enablea klocktillgång för portA
+/* 2. Enablea klocktillgång för portA. 
+(Krigutrustningens klocktillgång styrs av AHB1 klockaktiveringsregister
+(RCC_AHB1ENR) och AHB2 klockaktiveringsregister)*/
 
-RCC->AHB1ENR |= 0x01; //Atkiverar GPIOA genom att sätta bit 0 i AHB1ENR till 1.
+RCC->AHB1ENR |= 0x01; //Atkiverar GPIOA genom att sätta bit 0 (GPIOAEN: IO port A clock enable) i AHB1ENR till 1. bit 1 = B, bit 2 = C...
+
 
 // 3. Enablea pins relaterade till vald port, för alternativ funktion
-GPIOA->MODER &= ~0x00F0; //Rensar bitarna 4-7 för att förbereda pins PA2 och PA3
-  //& samt ~forcerar en invertering som leder till att bitarna som via hex betecknas med 1, i realvärde ersätts med 0
+GPIOA->MODER &= ~0x00F0; //Sätter bitarna 4-7 till 0 för att förbereda pins PA2 och PA3
+  //0x00F0 är 1111 0000 i binär.  '&' jämför enligt "and-logiken" och '~' inverterar bitarna vilket i slutändan gör att operationen endast påverkar bitarna 4-7 och sätter dessa till 0. Bitarna 0-3 förblir oförändrade
 
-GPIOA->MODER |= 0x00A0; //Sätter bitarna 5 och 7 till 1 för att aktivera alternativ funktionalitet på pins PA2 och PA3
+GPIOA->MODER |= 0x00A0; //0x00A0 är 1010 0000 i binär. Sätter bitarna 5 och 7 till 1 enligt or "or-logiken" som i sin tur ställer in pinsen PA2 och PA3 till "Alternate function mode". Eftersom bitarna 0-3 är 0 och vi använder 'or' förblir dessa oförändrade.
+/*00: Input (reset state)
+01: General purpose output mode
+10: Alternate function mode
+11: Analog mode*/
 
-// 4. Välja typen av alternativ funktion för de valde pinsen
+// 4. Välja typen av Alternate function för de valde pinsen. GPIOx_AFRL (for pin 0 to 7) and GPIOx_AFRH (for pin 8 to 15) 
 GPIOA->AFR[0] &= ~0xFF00; //Rensar bitarna 8-15 för att förbereda pins PA2 och PA3
 GPIOA->AFR[0] |= 0x7700; //Sätter vi bitarna 8-11 samt 12-15 till formatet 0111
 
